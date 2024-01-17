cfc960374bbca29948d80b06d5312bc9c4244d7b
kelche <kelcheone@gmail.com>
2024-01-17 11:52:06 +0300

    feat: bubble sort

--- src/__tests__/BubbleSort.ts
+++ src/__tests__/BubbleSort.ts
@@ -1,4 +1,4 @@
-import bubble_sort from "@code/BubbleSort";
+import bubble_sort from "../../scripts/code/BubbleSort";
 
 test("bubble-sort", function () {
     const arr = [9, 3, 7, 4, 69, 420, 42];
@@ -7,4 +7,3 @@ test("bubble-sort", function () {
     bubble_sort(arr);
     expect(arr).toEqual([3, 4, 7, 9, 42, 69, 420]);
 });
-
